b47143a5c88749741b8cb717d642667a2bfcffc8
Chandra Keshwar Jaiswal <134600960+Rudra554902y@users.noreply.github.com>
2026-04-15 09:43:15 +0530

    Delete 2515-shortest-distance-to-target-string-in-a-circular-array directory

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
-        mn=float('inf')
-        n=len(words)
-        for i,val in enumerate(words):
-            if val==target:
-                mn=min(mn,abs(i-startIndex),n-abs(i-startIndex))
-        return mn if mn!=float('inf') else -1
