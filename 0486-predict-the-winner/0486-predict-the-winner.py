c68226c4f9c8742d8944c88af439d9dbead8a054
Chandra Keshwar Jaiswal <134600960+Rudra554902y@users.noreply.github.com>
2026-08-01 08:39:51 +0530

    Time: 0 ms (100%), Space: 19.3 MB (79.49%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,29 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
+        # n = len(nums)
+        # dp = [[-1] * n for _ in range(n)]
+        # def solve(i, j):
+        #     if i == j:
+        #         return nums[i]
+
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+
+        #     dp[i][j] = max(nums[i] - solve(i + 1, j), nums[j] - solve(i, j - 1))
+
+        #     return dp[i][j]
+        # return solve(0, len(nums) - 1) >= 0
+
         n = len(nums)
-        dp = [[-1] * n for _ in range(n)]
-        def solve(i, j):
-            if i == j:
-                return nums[i]
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = nums[i]
 
-            if dp[i][j] != -1:
-                return dp[i][j]
-
-            dp[i][j] = max(nums[i] - solve(i + 1, j), nums[j] - solve(i, j - 1))
-
-            return dp[i][j]
-        return solve(0, len(nums) - 1) >= 0
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                takeLeft = nums[i] - dp[i + 1][j]
+                takeRight = nums[j] - dp[i][j - 1]
+                dp[i][j] = max(takeLeft, takeRight)
+        return dp[0][n-1] >= 0
 
